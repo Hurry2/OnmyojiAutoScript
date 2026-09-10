60b5c08860620b12f513e3433ed34e19b5374c6b
Hurry2 <1546296221@qq.com>
2026-09-10 17:55:02 +0800

    fix(RealmRaid):回撤修改

--- tasks/RealmRaid/script_task.py
+++ tasks/RealmRaid/script_task.py
@@ -535,17 +535,17 @@ class ScriptTask(GeneralBattle, GameUi, SwitchSoul, RealmRaidAssets):
         失败界面再次挑战
         :return: 是否再战成功
         """
-        self.wait_until_appear(self.I_REALM_FIRE_AGAIN)
+        self.wait_until_appear(self.I_FIRE_AGAIN)
         while True:
             self.screenshot()
-            if not self.appear(self.I_REALM_FIRE_AGAIN):
+            if not self.appear(self.I_FIRE_AGAIN):
                 logger.info(f'Click fire again success')
                 return True
             if self.appear_then_click(self.I_SHOW_AGAIN, interval=2):
                 continue
             if self.appear_then_click(self.I_FRESH_ENSURE, interval=2):
                 continue
-            if self.appear_then_click(self.I_REALM_FIRE_AGAIN, interval=2):
+            if self.appear_then_click(self.I_FIRE_AGAIN, interval=2):
                 continue
         return False
 
