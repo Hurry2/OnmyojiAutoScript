67c7303f7ed1ec4507c76f03b542c9f4bc922d3b
Hurry2 <1546296221@qq.com>
2026-09-24 15:59:59 +0800

    feat(一键协战):推送内容的服务器字段不足4字时对齐

--- tasks/AssistBattle/script_task.py
+++ tasks/AssistBattle/script_task.py
@@ -88,7 +88,7 @@ class ScriptTask(
         push_content.append(f"本次执行任务：")
         for result in results:
             # 账号取前4位，服务器取后4位，角色名取后2位
-            message = f"{result['account'][:4] + ('**' if len(result['account']) > 4 else '')}-{'安卓' if result['apple_or_android'] else '苹果'}-{result['svr'][-4:]}-{'**' + result['character'][-2:]}"
+            message = f"{result['account'][:4] + ('**' if len(result['account']) > 4 else '')}-{'安卓' if result['apple_or_android'] else '苹果'}-{str(result['svr'])[-4:]:　>4}-{'**' + result['character'][-2:]}"
             if self.conf.assist_battle_config.evozone_enable:
                 message += f"-觉醒{result['evozone_done']}次"
             if self.conf.assist_battle_config.realmraid_enable:
@@ -104,7 +104,7 @@ class ScriptTask(
         ):
             push_content.append(f"今日协战任务：")
             for result in results:
-                message = f"{result['account'][:4] + ('…' if len(result['account']) > 4 else '')}-{'安卓' if result['apple_or_android'] else '苹果'}-{result['svr'][-4:]}-{'…' + result['character'][-2:]}"
+                message = f"{result['account'][:4] + ('…' if len(result['account']) > 4 else '')}-{'安卓' if result['apple_or_android'] else '苹果'}-{str(result['svr'])[-4:]:　>4}-{'…' + result['character'][-2:]}"
                 if self.conf.assist_battle_config.evozone_enable:
                     message += f"-觉醒{result['evozone_final']}次"
                 if self.conf.assist_battle_config.realmraid_enable:
